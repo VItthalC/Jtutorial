22db9d07f6f6408a92d88b7e8bad73ec7e0eaaa3
VItthalC <69179597+VItthalC@users.noreply.github.com>
2020-08-07 00:21:45 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,12 @@
+package test;
+
+public class testPlan {
+	static String s2;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="test1",s2="tester1";
+int r = s.compareTo(s2);
+System.out.println(""+r);
+	}
+
+}
